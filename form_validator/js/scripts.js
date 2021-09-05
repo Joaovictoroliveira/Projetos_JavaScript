4b4aaff33b9039b8a0b0f5deddf593f03adb0a3b
Joao Victor <joao.gomes31@fatec.sp.gov.br>
2021-09-04 21:18:05 -0300

    Refatorando o javascript

--- form_validator/js/scripts.js
+++ form_validator/js/scripts.js
@@ -1,41 +1,58 @@
 let btn = document.querySelector(".btn-submit");
 
 function validaCampos() {
+    validaUsername();
+    validaEmail();
+    validaPassword();
+    validaConfirmPassword();
+}
+
+function validaUsername() {
     let username = document.querySelector("#username");
-    let email = document.querySelector("#email");
-    let password = document.querySelector("#password");
-    let confirm_password = document.querySelector("#confirm-password");
-
     let vm_username = document.querySelector(".vm-username");
-    let vm_email = document.querySelector(".vm-email");
-    let vm_password = document.querySelector(".vm-password");
-    let vm_confirm_password = document.querySelector(".vm-confirm-password");
 
-    if(username.value == null || username.value == '' || username.value.length <= 3) {
+    if(username.value == null || username.value == '' || username.value.length <= 2) {
         username.style.border = '2px solid red';
         vm_username.style.display='block';
     } else {
         username.style.border = '2px solid green';
         vm_username.style.display='none';
     }
+}
 
-    if(email.value == null || email.value == '' || email.value.length <= 3) {
+function validaEmail() {
+    let email = document.querySelector("#email");
+    let vm_email = document.querySelector(".vm-email");
+    let re = /\S+@\S+\.\S+/;
+
+    if(email.value == null || email.value == '') {
         email.style.border = '2px solid red';
         vm_email.style.display='block';
     } else {
         email.style.border = '2px solid green';
         vm_email.style.display='none';
     }
+}
 
-    if(password.value == null || password.value == '' || password.value.length <= 3) {
+function validaPassword() {
+    let password = document.querySelector("#password");
+    let vm_password = document.querySelector(".vm-password");    
+
+    if(password.value == null || password.value == '' || password.value.length <= 5) {
         password.style.border = '2px solid red';
         vm_password.style.display='block';
     } else {
         password.style.border = '2px solid green';
         vm_password.style.display='none';
     }
+}
 
-    if(confirm_password.value == null || confirm_password.value == '' || confirm_password.value.length <= 3) {
+function validaConfirmPassword() {
+    let confirm_password = document.querySelector("#confirm-password");
+    let vm_confirm_password = document.querySelector(".vm-confirm-password");
+    let password = document.querySelector("#password");
+
+    if(confirm_password.value == null || confirm_password.value == '' || confirm_password.value.length <= 5 || (confirm_password.value != password.value)) {
         confirm_password.style.border = '2px solid red';
         vm_confirm_password.style.display='block';
     } else {
@@ -43,6 +60,5 @@ function validaCampos() {
         vm_confirm_password.style.display='none';
     }
 }
-
 
 btn.addEventListener('click', validaCampos);
